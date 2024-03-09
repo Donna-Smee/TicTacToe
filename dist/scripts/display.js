10a4df09e7935a3d2dc79c578ca01b987d48fa04
Donna-Smee <dailydosesofdonuts@gmail.com>
2024-03-08 23:24:19 -0500

    Initialized Website, Added start game functionality for single player

---
+++ dist/scripts/display.js
@@ -0,0 +1,20 @@
+
+// hide the start screen
+function hideStartScreen(){
+    $("#start-screen").hide();
+}
+
+// show the choose X or O screen
+function showChooseXOScreen(){
+    $("#chooseXO-screen").css("display", "flex");
+}
+
+// hide the choose X or O screen
+function hideChooseXOScreen(){
+    $("#chooseXO-screen").hide();
+}
+
+
+function showGameBoard(){
+    $("#game-board").css("display", "flex");
+}
